ab752c910fc927aa10ed4b8899fca1fe7ec7bd24
muduimushui <1109005106@qq.com>
2019-12-06 20:59:28 +0800

    ros sub and pub by matlab

--- drawARM.m
+++ drawARM.m
@@ -1,4 +1,38 @@
-function drawARM(info)
+%%
+function main
+rosdata
+end
+
+function rosdata
+global pos 
+global orient
+robotpose_sub = rossubscriber('/pose',@doit)
+pause(2)
+% cc = robotpose_sub.receive
+data = receive(robotpose_sub,10)
+pos = data.Linear
+orient = data.Angular
+pause(1)
+
+end
+
+%%
+function pub
+
+while (true)
+pub = rospublisher('/mytalk','std_msgs/String');
+msg = rosmessage(pub)
+msg.Data = 'matlab'
+send(pub,msg)
+% latchpub = rospublisher('mytalk','IsLatching',true)
+pause(5)
+end 
+end
+
+
+
+%% 
+function [] = draw()
 thetax(1)=25;thetay(1)=0;
 thetax(2)=25;thetay(2)=0;
 thetax(3)=10;thetay(3)=0;
@@ -32,5 +66,32 @@ jointl=6.5;
             thetax(7),thetay(7),thetax(8),thetay(8),thetax(9),thetay(9),thetax(10),thetay(10),thetax(11),thetay(11),thetax(12),thetay(12),Pbx,Pby,Pbz,0); 
 axis equal
 end
-% 
+%% 
+function doit(a,b) %/pose有两个值输入进来,一个是topic属性,一个是message信息
+global pos 
+pos;
+a
+b
+b.Linear.X
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
