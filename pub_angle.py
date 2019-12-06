f36c43ee96318a7ce4e315a3a133c8728a436f6c
muduimushui <1109005106@qq.com>
2019-12-06 22:46:19 +0800

    final pub msg

--- pub_angle.py
+++ pub_angle.py
@@ -4,34 +4,31 @@ from std_msgs.msg import Int32
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseArray
 import time 
-
+import numpy as np
 
 def talker():
-	pub=rospy.Publisher("/detect_info",PoseArray,queue_size = 10)
+	pub=rospy.Publisher("/angle_info",PoseArray,queue_size = 10)
 	rospy.init_node("sender",anonymous=True)
 	rate=rospy.Rate(5)
-
-	posedata1 = Pose()
-	posedata2 = Pose()
-	total_info = PoseArray()
-
+	# total_info.poses= [posedata1]*12
 	while not rospy.is_shutdown():
-		posedata1.orientation.x = 1
-		posedata1.orientation.y = 2
-		posedata1.orientation.z = 3
-		posedata1.position.x = 2.5
-
-
-		posedata2.orientation.x= -1
-		posedata2.orientation.y = -2
-		posedata2.orientation.z = -3
-		posedata2.position.x = -2.5
-
+		total_info = PoseArray() #init before using !!!!!!
+		# posedata.orientation.x = 1
+		# posedata.position.x = 2.5
+		oridata = np.random.rand(24).reshape(12,2)
+		oridata = (oridata-1) / 3.14 *180
+		for i in range(12):
+			posedata = Pose()  #init before using !!!!!!
+			posedata.orientation.x = oridata[i,0]
+			posedata.orientation.y = oridata[i,1]
+			total_info.poses.append(posedata)
+			# total_info.poses[i].orientation.x = oridata[i,0]
+			# total_info.poses[i].orientation.y = oridata[i,1]
+		print('send data ******************************************************')
 		print(total_info)
-		total_info.poses = [posedata1,posedata2]
-
 		pub.publish(total_info)
-		rospy.loginfo('send data')
+		# rospy.loginfo('send data ******************************************************')
+
 		rate.sleep()
 
 if __name__=='__main__':
